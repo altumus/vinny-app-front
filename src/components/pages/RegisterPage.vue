4f75dbb85943093b46276a132c92f80178f88217
Егор Пантелеев <kakeparake@gmail.com>
2024-06-21 16:56:43 +0300

    fix: id element

--- src/components/pages/RegisterPage.vue
+++ src/components/pages/RegisterPage.vue
@@ -57,7 +57,7 @@
                 v-model="formData.confirmPassword"
                 type="confirm-password"
                 name="confirm-password"
-                id="password"
+                id="confirm-password"
                 placeholder="••••••••"
                 class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                 required
