a84e8a6a43efee6797c7c3d4733f4e1591ab67fe
Егор Пантелеев <kakeparake@gmail.com>
2024-06-21 17:48:29 +0300

    feat: reset tags filter

--- src/components/tags/TagsList.vue
+++ src/components/tags/TagsList.vue
@@ -1,13 +1,14 @@
 <template>
   <div
-    className="w-full md:w-fit min-w-[250px] p-[20px] h-fit flex flex-col gap-[5px] rounded-[8px] shadow-lg border-[2px] border-gray-700 overflow-hidden"
+    class="w-full md:w-fit min-w-[250px] p-[20px] h-fit flex flex-col gap-[5px] rounded-[8px] shadow-lg border-[2px] border-gray-700 overflow-hidden"
   >
-    <p className="font-[700] text-[19px]">Tags</p>
+    <p class="font-[700] text-[19px]">Tags</p>
     <div
       @click="selectTag(tag)"
       v-for="(tag, idx) in tags"
-      className="hover:bg-gray-200 cursor-pointer px-[5px] py-[5px] transition duration-200 rounded-[6px]"
+      class="hover:bg-gray-200 cursor-pointer px-[5px] py-[5px] transition duration-200 rounded-[6px]"
       :key="idx"
+      :class="{ 'bg-green-200': $route.query.tag === tag }"
     >
       # {{ tag }}
     </div>
@@ -23,10 +24,14 @@ export default {
   },
   methods: {
     selectTag(tag) {
+      let val = tag
+      if (this.$route.query.tag === tag) {
+        val = undefined
+      }
       this.$router.push({
         name: this.$route.name,
         query: {
-          tag: tag
+          tag: val
         }
       })
     }
